b5924b88446dcbd4a687cb6a3eceb12928965d22
Nina Pypchenko <22447785+nina-py@users.noreply.github.com>
2021-04-27 15:05:45 +1000

    Fix linting errors and warnings in custom hook

--- collections/src/hooks/useNotifications.ts
+++ collections/src/hooks/useNotifications.ts
@@ -1,12 +1,20 @@
 import React, { useState } from 'react';
 
+interface useNotificationsReturnValues {
+  open: boolean;
+  message: string;
+  hasError: boolean;
+  showNotification: (message: string, isError: boolean) => void;
+  handleClose: (event?: React.SyntheticEvent, reason?: string) => void;
+}
+
 /**
  * The Notification component needs its state managed
  * from the next level up (at least), and so as not to repeat ourselves
  * on every page where the Notification component is used, setting
  * up state vars and helper methods for it happens within a custom hook
  */
-export const useNotifications = () => {
+export const useNotifications = (): useNotificationsReturnValues => {
   // These are used to display or hide notifications with messages from the API
   const [open, setOpen] = useState<boolean>(false);
   const [message, setMessage] = useState<string>('');
@@ -16,7 +24,7 @@ export const useNotifications = () => {
    * Show a notification to the user whether the action (such as saving a record)
    * has completed successfully.
    */
-  const showNotification = (message: string, isError: boolean = false) => {
+  const showNotification = (message: string, isError = false): void => {
     setHasError(isError);
     setMessage(message);
     setOpen(true);
@@ -25,7 +33,7 @@ export const useNotifications = () => {
   /**
    * Close the toast notification
    */
-  const handleClose = (event?: React.SyntheticEvent, reason?: string) => {
+  const handleClose = (event?: React.SyntheticEvent, reason?: string): void => {
     if (reason === 'clickaway') {
       return;
     }
