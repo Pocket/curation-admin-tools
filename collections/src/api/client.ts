6b631ad1a6c6624e2849829a899c2d52cdc37333
Daniel Brooks <bass_rock@me.com>
2021-04-28 07:38:04 -0700

    fix(variables): review feedback

--- collections/src/api/client.ts
+++ collections/src/api/client.ts
@@ -4,7 +4,7 @@ const apolloOptions = {
   cache: new InMemoryCache(),
   name: `CurationAdminToolsCollections`,
   version: `${process.env.REACT_APP_GIT_SHA ?? 'local'}`,
-}
+};
 
 export const client = new ApolloClient({
   uri: `${process.env.REACT_APP_COLLECTION_API_ENDPOINT}`,
