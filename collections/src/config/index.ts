9e3fb9efd223e0d841717c7970c06a0234d3bb9f
Daniel Brooks <bass_rock@me.com>
2021-04-30 10:32:38 -0700

    fix(variables): node env gets overwrittten with react scripts

--- collections/src/config/index.ts
+++ collections/src/config/index.ts
@@ -1,4 +1,4 @@
-const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = process.env.REACT_APP_ENV === 'production';
 
 export const config = {
   apolloClientName: `CurationAdminToolsCollections`,
