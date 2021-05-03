c5f65d798701136e342b539f32bb350c06340a0b
Daniel Brooks <bass_rock@me.com>
2021-05-03 08:05:27 -0700

    fix(variables): adding parentheses

--- collections/src/config/index.ts
+++ collections/src/config/index.ts
@@ -5,9 +5,10 @@ export const config = {
   version: `${process.env.REACT_APP_GIT_SHA ?? 'local'}`,
   //If we have an env variable set, lets use it. Otherwise if we are production, lets you the prod api, otherwise use the dev api
   collectionApiEndpoint: `${
-    process.env.REACT_APP_COLLECTION_API_ENDPOINT ?? isProduction
+    process.env.REACT_APP_COLLECTION_API_ENDPOINT ??
+    (isProduction
       ? 'https://collection-api.readitlater.com/admin'
-      : 'https://collection-api.getpocket.dev/admin'
+      : 'https://collection-api.getpocket.dev/admin')
   }`,
   //Client api only exists on production
   clientApiEndpoint: `${
