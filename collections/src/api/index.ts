5d177fe2ccc97ffb41f91b059543eab16ee898fa
Nina Pypchenko <22447785+nina-py@users.noreply.github.com>
2021-05-20 00:19:31 +1000

    [BACK-823] Use updateCollectionStorySortOrder mutation (#50)

--- collections/src/api/index.ts
+++ collections/src/api/index.ts
@@ -33,6 +33,7 @@ export {
   useCreateCollectionStoryMutation,
   useUpdateCollectionStoryMutation,
   useDeleteCollectionStoryMutation,
+  useUpdateCollectionStorySortOrderMutation,
   useGetSearchCollectionsLazyQuery,
   useGetCollectionStoriesQuery,
   useImageUploadMutation,
