8572ffa67142bc8ed68f8b2da8b09ad84356c4a1
Nina Pypchenko <22447785+nina-py@users.noreply.github.com>
2021-06-09 04:29:55 +1000

    [BACK-822] Implement a Preview page for collections (#71)
    - Added a new "Preview Collection" button - the eye icon right under
    the edit icon in the top right corner of the collection page.
    
    - Added a new CollectionPreview component with tests. It is used
    on the Collection page to display a preview for the entire collection
    in a modal. The styles are rudimentary and fonts don't match the web
    version - something to iterate on later.

---
+++ collections/src/components/CollectionPreview/CollectionPreview.tsx
@@ -0,0 +1,138 @@
+import React from 'react';
+import { Card, Grid, Hidden, Typography } from '@material-ui/core';
+import ReactMarkdown from 'react-markdown';
+import {
+  StoryModel,
+  CollectionModel,
+  AuthorModel,
+} from '../../api/collection-api';
+import { useStyles } from './CollectionPreview.styles';
+import { CollectionStoryAuthor } from '../../api/collection-api/generatedTypes';
+
+interface CollectionPreviewProps {
+  /**
+   * An object with everything collection-related in it.
+   */
+  collection: CollectionModel;
+
+  /**
+   * Collection stories, separately
+   */
+  stories: StoryModel[] | undefined;
+}
+
+export const CollectionPreview: React.FC<CollectionPreviewProps> = (
+  props
+): JSX.Element => {
+  const { collection, stories } = props;
+  const classes = useStyles();
+
+  // The &middot; character is only needed if the story has authors as it separates
+  // the list of authors and the name of the publisher.
+  // There appears to be no way to display an HTML special character conditionally
+  // (well, except for setting innerHTML directly) other than assigning it to a variable
+  const middot = '\u00b7';
+
+  return (
+    <>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Typography variant="h4" color="primary">
+            Collection Preview
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="h4" className={classes.title}>
+            {collection.title}
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <ReactMarkdown className={classes.excerpt}>
+            {collection.excerpt}
+          </ReactMarkdown>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography
+            variant="subtitle1"
+            color="textSecondary"
+            component="span"
+            align="left"
+          >
+            {collection.authors
+              .map((author: AuthorModel) => {
+                return author.name;
+              })
+              .join(', ')}
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <img width="100%" src={collection.imageUrl} alt={collection.title} />
+        </Grid>
+        <Grid item xs={12}>
+          <ReactMarkdown className={classes.intro}>
+            {collection.intro}
+          </ReactMarkdown>
+        </Grid>
+        {stories &&
+          stories.map((story: StoryModel) => {
+            // TODO: decouple StoryListCard from the form so that
+            // it can be used here, too
+
+            // Work out a comma-separated list of authors if there are any for this story
+            const displayAuthors = story.authors
+              ?.map((author: CollectionStoryAuthor) => {
+                return author.name;
+              })
+              .join(', ');
+
+            return (
+              <Card
+                variant="outlined"
+                square
+                className={classes.storyCard}
+                key={story.externalId}
+              >
+                <Grid container spacing={2}>
+                  <Grid item xs={3}>
+                    <img
+                      src={story.imageUrl}
+                      width="100%"
+                      alt={story.title}
+                      className={classes.image}
+                    />
+                  </Grid>
+                  <Grid item xs={7} sm={8}>
+                    <Typography
+                      variant="h3"
+                      align="left"
+                      className={classes.storyTitle}
+                      gutterBottom
+                    >
+                      <a href={story.url}>{story.title}</a>
+                    </Typography>
+                    <Typography
+                      variant="subtitle1"
+                      color="textSecondary"
+                      component="span"
+                      align="left"
+                    >
+                      <span>{displayAuthors}</span>
+                      {displayAuthors.length > 0 && ` ${middot} `}
+                      <span>{story.publisher}</span>
+                    </Typography>
+                    <Hidden smDown implementation="css">
+                      <Typography component="div">
+                        <ReactMarkdown className="compact-markdown">
+                          {story.excerpt ? story.excerpt : ''}
+                        </ReactMarkdown>
+                      </Typography>
+                    </Hidden>
+                  </Grid>
+                </Grid>
+              </Card>
+            );
+          })}
+      </Grid>
+    </>
+  );
+};
