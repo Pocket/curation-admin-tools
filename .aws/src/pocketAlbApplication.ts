1c58610610f7f8d4a2dcc9064c19bc5aec5d6e7c
Nina Pypchenko <22447785+nina-py@users.noreply.github.com>
2021-07-29 10:25:53 +1000

    Replace 'this' with 'scope' inside function

--- .aws/src/pocketAlbApplication.ts
+++ .aws/src/pocketAlbApplication.ts
@@ -50,7 +50,7 @@ export function createApplicationCodePipeline(
   scope: Construct,
   app: PocketALBApplication
 ) {
-  new PocketECSCodePipeline(this, 'code-pipeline', {
+  new PocketECSCodePipeline(scope, 'code-pipeline', {
     prefix: config.prefix,
     source: {
       codeStarConnectionArn: config.codePipeline.githubConnectionArn,
