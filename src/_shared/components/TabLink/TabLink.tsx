ac5b827786e3b461ee801cf0d86016c154728618
Nina Pypchenko <22447785+nina-py@users.noreply.github.com>
2021-09-15 09:43:28 +1000

    [BACK-1039] Finish frontend refactor to support multiple tools (#316)
    - Move app from /collections to root folder.
    
    - Update dependabot, linter, unit test workflows.
    
    - Update Dockerfile, TypeScript config (to the one compatible with React
    that was used inside the app), merge README files.
    
    - Enabled eslint-plugin-react with default configuration.
    
    - Disabled the indent check as indentation is already enforced by the Prettier plugin
    for ESLint - and the backend code config is not compatible with indented JSX code
    that Prettier produces.
    
    - Fixed errors (all but one type that has had to have its rule disabled for the time being)
    and reduced warnings to a manageable level.
    
    - Incorporated the fix by @jpetto to get production build working.

---
+++ src/_shared/components/TabLink/TabLink.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Link, LinkProps } from 'react-router-dom';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
+import { CircularProgress } from '@material-ui/core';
+import { Chip } from '../';
+import { curationPalette } from '../../../theme';
+
+interface TabLinkProps {
+  /**
+   * Optional number of articles loaded on the tab.
+   */
+  count?: number;
+
+  /**
+   * Whether the current tab is active.
+   */
+  tabSelected: boolean;
+}
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    spinner: {
+      color: curationPalette.white,
+    },
+  })
+);
+
+/**
+ * Used for routing between various subpages available as tabs, i.e.
+ * Prospects/Snoozed/Approved/Rejected. Optionally shows a Chip with
+ * the number of articles available on that tab.
+ */
+// eslint-disable-next-line react/display-name
+export const TabLink = React.forwardRef<
+  HTMLAnchorElement,
+  LinkProps & TabLinkProps
+>((props, ref): JSX.Element => {
+  const classes = useStyles();
+  const { count, children, tabSelected, ...otherProps } = props;
+  const showChip = !!(count && count > 0);
+
+  // workaround to make sure the chip on the active tab is highlighted.
+  const color = tabSelected ? 'primary' : 'default';
+
+  // don't show the exact number of articles if there is more
+  // than a thousand of entries
+  const chipLabel = count && count > 999 ? '999+' : count;
+
+  return (
+    <Link {...otherProps} ref={ref}>
+      {children}
+      <Chip
+        label={
+          showChip ? (
+            chipLabel
+          ) : (
+            <CircularProgress size={14} className={classes.spinner} />
+          )
+        }
+        size="small"
+        color={color}
+      />
+    </Link>
+  );
+});
