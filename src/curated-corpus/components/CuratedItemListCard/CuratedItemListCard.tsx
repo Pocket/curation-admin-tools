426cbfba6e1cbc384c30dee99b20cd415f6e7b10
Nina Pypchenko <22447785+nina-py@users.noreply.github.com>
2021-10-28 21:51:59 +1100

    Revert unnecessary changes to CuratedItemCard

--- src/curated-corpus/components/CuratedItemListCard/CuratedItemListCard.tsx
+++ src/curated-corpus/components/CuratedItemListCard/CuratedItemListCard.tsx
@@ -32,78 +32,76 @@ export const CuratedItemListCard: React.FC<CuratedItemListCardProps> = (
   const { item } = props;
 
   return (
-    <>
-      <Card className={classes.root}>
-        <CardMedia
-          component="img"
-          src={
-            item.imageUrl && item.imageUrl.length > 0
-              ? item.imageUrl
-              : '/placeholders/collectionSmall.svg'
-          }
-          alt={item.title}
-        />
-        <CardContent className={classes.content}>
-          <Typography className={classes.publisher} gutterBottom>
-            {item.publisher}
-          </Typography>
-          <Typography
-            className={classes.title}
-            variant="h3"
-            align="left"
-            gutterBottom
-          >
-            <Link href={item.url} className={classes.link}>
-              {item.title}
-            </Link>
-          </Typography>
-          <Typography variant="body2" component="p" gutterBottom>
-            {item.excerpt}
-          </Typography>
-        </CardContent>
+    <Card className={classes.root}>
+      <CardMedia
+        component="img"
+        src={
+          item.imageUrl && item.imageUrl.length > 0
+            ? item.imageUrl
+            : '/placeholders/collectionSmall.svg'
+        }
+        alt={item.title}
+      />
+      <CardContent className={classes.content}>
+        <Typography className={classes.publisher} gutterBottom>
+          {item.publisher}
+        </Typography>
+        <Typography
+          className={classes.title}
+          variant="h3"
+          align="left"
+          gutterBottom
+        >
+          <Link href={item.url} className={classes.link}>
+            {item.title}
+          </Link>
+        </Typography>
+        <Typography variant="body2" component="p" gutterBottom>
+          {item.excerpt}
+        </Typography>
+      </CardContent>
 
-        {/* Push the rest of the elements to the bottom of the card. */}
-        <div className={classes.flexGrow} />
+      {/* Push the rest of the elements to the bottom of the card. */}
+      <div className={classes.flexGrow} />
 
-        <List dense className={classes.list}>
-          <ListItem>
-            <ListItemIcon className={classes.listItemIcon}>
-              <ThumbUpIcon />
-            </ListItemIcon>
-            <ListItemText
-              className={classes.status}
-              primary={item.status.toLowerCase()}
-            />
-          </ListItem>
-          <ListItem>
-            <ListItemIcon className={classes.listItemIcon}>
-              <LabelOutlinedIcon />
-            </ListItemIcon>
-            <ListItemText
-              className={classes.topic}
-              primary={item.topic.toLowerCase()}
-            />
-          </ListItem>
-          <ListItem>
-            <ListItemIcon className={classes.listItemIcon}>
-              <LanguageIcon />
-            </ListItemIcon>
-            <ListItemText primary={item.language.toUpperCase()} />
-          </ListItem>
-        </List>
+      <List dense className={classes.list}>
+        <ListItem>
+          <ListItemIcon className={classes.listItemIcon}>
+            <ThumbUpIcon />
+          </ListItemIcon>
+          <ListItemText
+            className={classes.status}
+            primary={item.status.toLowerCase()}
+          />
+        </ListItem>
+        <ListItem>
+          <ListItemIcon className={classes.listItemIcon}>
+            <LabelOutlinedIcon />
+          </ListItemIcon>
+          <ListItemText
+            className={classes.topic}
+            primary={item.topic.toLowerCase()}
+          />
+        </ListItem>
+        <ListItem>
+          <ListItemIcon className={classes.listItemIcon}>
+            <LanguageIcon />
+          </ListItemIcon>
+          <ListItemText primary={item.language.toUpperCase()} />
+        </ListItem>
+      </List>
 
-        <CardActions className={classes.actions}>
-          <Button buttonType="positive" variant="text">
-            Schedule
-          </Button>
-          <Button buttonType="negative" variant="text">
-            Reject
-          </Button>
-          <Button buttonType="positive" variant="text">
-            Edit
-          </Button>
-        </CardActions>
-      </Card>{' '}
-    </>
+      <CardActions className={classes.actions}>
+        <Button buttonType="positive" variant="text">
+          Schedule
+        </Button>
+        <Button buttonType="negative" variant="text">
+          Reject
+        </Button>
+        <Button buttonType="positive" variant="text">
+          Edit
+        </Button>
+      </CardActions>
+    </Card>
   );
 };
