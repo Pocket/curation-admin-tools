133ffc3246ed5db653c8390c7b40b0a04a5c68c2
Herraj Luhano <herrajluhano@gmail.com>
2022-01-14 12:38:30 -0500

    feature(prospectingPage): [BACK-1152] add empty state component for prospecting page (#644)
    * add empty state component for prospecting page
    
    * remove console log

---
+++ src/curated-corpus/pages/NewTabCurationPage/EmptyState.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const EmptyState: React.FC = () => {
+  return (
+    <>
+      <h1>Boom! Curated!</h1>
+      <img src="/placeholders/prospectsEmptyState.gif" />
+    </>
+  );
+};
