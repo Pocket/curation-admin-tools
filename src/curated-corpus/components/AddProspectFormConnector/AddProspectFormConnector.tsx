92e85fb01920cab4c80ccaba2bf265f69988232a
Nina Pypchenko <22447785+nina-py@users.noreply.github.com>
2022-03-04 02:30:26 +1100

    chore(refactor): [BACK-1330] Refactor manual scheduling workflow to fix a bug (#750)
    * chore(refactor): [BACK-1330] Refactor manual scheduling workflow to fix a bug
    
    - Manually added and scheduled items have had its own separate workflow
    set up that has a lot in common with the workflow for prospects on the Prospecting
    page. Uniting these two workflows into one fixes a UI bug with scheduling -
    stories added manually and scheduling straight away do not appear on the sidebar
    if they've been scheduled for the next couple of days.
    
    This initial commit refactors all of the business logic out of the `AddProspectForm`
    component into a new `AddProspectFormConnector` component, leaving the form component
    to be in charge of the form layout only.
    
    * Fix error overlay freeze during local development
    
    * WIP: combine the two workflows
    
    * - Updated the code so that adding a manual item workflow delegates
    back to the main workflow on the Prospecting page when metadata
    has been fetched for a URL.

---
+++ src/curated-corpus/components/AddProspectFormConnector/AddProspectFormConnector.tsx
@@ -0,0 +1,118 @@
+import React, { useState } from 'react';
+import { FormikHelpers, FormikValues } from 'formik';
+import {
+  Prospect,
+  useGetApprovedItemByUrlLazyQuery,
+  useGetUrlMetadataLazyQuery,
+} from '../../../api/generatedTypes';
+import { AddProspectForm } from '../';
+import { useNotifications } from '../../../_shared/hooks';
+import { transformUrlMetaDataToProspect } from '../../helpers/helperFunctions';
+
+interface AddProspectFormConnectorProps {
+  /**
+   * Toggle the AddProspectModal to show/hide as necessary.
+   */
+  toggleModal: VoidFunction;
+
+  /**
+   * Toggle the modal that contains the ApprovedItem form as necessary.
+   */
+  toggleApprovedItemModal: VoidFunction;
+
+  /**
+   * The Prospecting page holds the prospect under consideration (its data being
+   * fed to either ApprovedItem or RejectedItem forms while the curator is editing)
+   * in the `currentProspect` state variable. We get the setter for this state
+   * variable from the page-level component to be able to update it
+   * when the manually added prospect is ready to be saved.
+   */
+  setCurrentProspect: (currentProspect: Prospect) => void;
+}
+
+/**
+ * This component contains all the business logic for the AddProspectForm workflow.
+ *
+ * @param props
+ * @constructor
+ */
+export const AddProspectFormConnector: React.FC<
+  AddProspectFormConnectorProps
+> = (props) => {
+  const { toggleModal, toggleApprovedItemModal, setCurrentProspect } = props;
+
+  // state variable to store the itemUrl field from the form
+  const [itemUrl, setItemUrl] = useState<string>('');
+
+  // set up some hooks
+  const { showNotification } = useNotifications();
+
+  /**
+   * Run through a series of steps when a Prospect is submitted manually
+   *
+   * @param values
+   * @param formikHelpers
+   */
+  const onSubmit = (
+    values: FormikValues,
+    formikHelpers: FormikHelpers<any>
+  ) => {
+    setItemUrl(values.itemUrl);
+    // This kicks off Step 1 below.
+    getApprovedItemByUrl({
+      variables: {
+        url: values.itemUrl,
+      },
+    });
+
+    formikHelpers.resetForm();
+  };
+
+  // Step 1: when a URL is submitted, the first step is to check if it is already in the corpus.
+  const [getApprovedItemByUrl] = useGetApprovedItemByUrlLazyQuery({
+    notifyOnNetworkStatusChange: true,
+    fetchPolicy: 'no-cache',
+    onCompleted: (data) => {
+      const approvedItem = data?.getApprovedCuratedCorpusItemByUrl;
+
+      // show error toast if the url exists already
+      if (approvedItem) {
+        showNotification('This URL already exists in the Corpus', 'error');
+        return;
+      }
+
+      // fetch metadata for url if it doesn't exist in the corpus
+      if (approvedItem === null) {
+        // This leads us to Step 2 below.
+        getUrlMetadata({
+          variables: {
+            url: itemUrl,
+          },
+        });
+      }
+    },
+  });
+
+  // Step 2: If the item is not yet in the corpus, get available metadata from it
+  // from the Parser.
+  const [getUrlMetadata] = useGetUrlMetadataLazyQuery({
+    notifyOnNetworkStatusChange: true,
+    fetchPolicy: 'no-cache',
+    onCompleted: (data) => {
+      // create a Prospect object from the URL metadata to be consumed by
+      // the ApprovedItem form
+      const prospect = transformUrlMetaDataToProspect(data.getUrlMetadata);
+
+      // set state variable so that it can be used by the ApprovedItem form
+      setCurrentProspect(prospect);
+
+      // Hide the AddProspect form
+      toggleModal();
+      // Show the ApprovedItem form and delegate back to the Prospecting page
+      // from this point onwards
+      toggleApprovedItemModal();
+    },
+  });
+
+  return <AddProspectForm onCancel={toggleModal} onSubmit={onSubmit} />;
+};
--- src/curated-corpus/components/AddProspectModal/AddProspectModal.tsx
+++ src/curated-corpus/components/AddProspectModal/AddProspectModal.tsx
@@ -1,22 +1,41 @@
-import { Grid } from '@material-ui/core';
 import React from 'react';
-import { AddProspectForm } from '..';
-
-import { SharedFormButtonsProps, Modal } from '../../../_shared/components';
+import { Grid } from '@material-ui/core';
+import { Prospect } from '../../../api/generatedTypes';
+import { AddProspectFormConnector } from '..';
+import { Modal } from '../../../_shared/components';
 
 interface AddProspectModalProps {
+  /**
+   * Whether the modal is visible on the screen or not.
+   */
   isOpen: boolean;
 
+  /**
+   * Toggle the AddProspectModal to show/hide as necessary.
+   */
   toggleModal: VoidFunction;
+
+  /**
+   * Toggle the modal that contains the ApprovedItem form as necessary.
+   */
+  toggleApprovedItemModal: VoidFunction;
+
+  /**
+   * A setter for the current prospect to be worked on. This is passed
+   * through straight to the AddProspectFormConnector component where you will
+   * find a detailed explanation why it's needed there.
+   */
+  setCurrentProspect: (currentProspect: Prospect) => void;
 }
 
 /**
- * Parent component for the AddProspectForm component
+ * Parent component for the AddProspectFormConnector component
  */
-export const AddProspectModal: React.FC<
-  AddProspectModalProps & SharedFormButtonsProps
-> = (props): JSX.Element => {
-  const { isOpen, toggleModal } = props;
+export const AddProspectModal: React.FC<AddProspectModalProps> = (
+  props
+): JSX.Element => {
+  const { isOpen, toggleModal, setCurrentProspect, toggleApprovedItemModal } =
+    props;
 
   return (
     <Modal open={isOpen} handleClose={toggleModal}>
@@ -25,9 +44,10 @@ export const AddProspectModal: React.FC<
           <h2>Add a New Curated Item</h2>
         </Grid>
         <Grid item>
-          <AddProspectForm
-            onCancel={toggleModal}
-            toggleAddProspectModal={toggleModal}
+          <AddProspectFormConnector
+            toggleModal={toggleModal}
+            toggleApprovedItemModal={toggleApprovedItemModal}
+            setCurrentProspect={setCurrentProspect}
           />
         </Grid>
       </Grid>
