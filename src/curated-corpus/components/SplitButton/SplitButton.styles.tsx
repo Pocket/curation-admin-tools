07e88ebcf144b9be1f2bed070bbdd74809b08b9a
Herraj Luhano <hluhano@mozilla.com>
2022-03-08 14:21:40 -0500

    fix(schedule item card): [BACK-1358] lower z-index to un block scheduled surface selector dropdown (#764)
    * fix(schedule item card): [BACK-1358] lower z-index to un block schedule surface selector dropdown

--- src/curated-corpus/components/SplitButton/SplitButton.styles.tsx
+++ src/curated-corpus/components/SplitButton/SplitButton.styles.tsx
@@ -5,6 +5,11 @@ import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
  */
 export const useStyles = makeStyles((theme: Theme) =>
   createStyles({
+    popper: {
+      // this prevents the dropdown from being covered by schedule item card
+      zIndex: 1,
+    },
+
     optionNameSmall: {
       minWidth: '4rem',
     },
