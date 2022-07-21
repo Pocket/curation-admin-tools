6ccdb823ee4f39e90714b7b837f09e88c56f86e8
Nina Pypchenko <22447785+nina-py@users.noreply.github.com>
2022-07-21 10:26:35 +1000

    Add test stubs

--- src/curated-corpus/pages/RejectedPage/RejectedPage.test.tsx
+++ src/curated-corpus/pages/RejectedPage/RejectedPage.test.tsx
@@ -3,12 +3,13 @@ import { MockedProvider, MockedResponse } from '@apollo/client/testing';
 import { RejectedPage } from './RejectedPage';
 import { render, screen, waitFor } from '@testing-library/react';
 import { mock_AllRejectedItems } from '../../integration-test-mocks/getRejectedItems';
-import userEvent from '@testing-library/user-event';
 
 describe('The RejectedPage', () => {
   let mocks: MockedResponse[] = [];
 
-  it('renders all rejected items on initial load', async () => {
+  it('renders page basics on initial load', async () => {
+    // Note: this test requires mocks for the rejected items query
+    // and appears to run it, but doesn't display rejected items on the page.
     mocks = [mock_AllRejectedItems];
 
     // Wrap the render call in a `waitFor` call to make sure
@@ -26,21 +27,25 @@ describe('The RejectedPage', () => {
 
     // How about the form?
     expect(screen.getByRole('form')).toBeInTheDocument();
-
-    // And the grid with rejected item cards? Nope
-
-    // test test test
-    // let's interact with this thing
-    const titleField = screen.getByLabelText(/filter by title/i);
-    userEvent.type(titleField, 'story');
-    await waitFor(() => {
-      userEvent.click(screen.getByRole('button', { name: 'Search' }));
-    });
-
-    const resetButton = screen.getByRole('button', { name: 'Reset Filters' });
-    expect(resetButton).toBeInTheDocument();
-    await waitFor(() => {
-      userEvent.click(resetButton);
-    });
   });
+
+  it.todo('shows the number of rejected items displayed on initial load');
+
+  it.todo('renders a full list of rejected items on initial load');
+
+  it.todo('renders rejected items filtered by title');
+
+  it.todo('renders rejected items filtered by topic');
+
+  it.todo('renders rejected items filtered by language');
+
+  it.todo('renders rejected items with a combination of filters applied');
+
+  it.todo(
+    'displays a full list of items after filtering and resetting filters'
+  );
+
+  it.todo('shows the next page of results');
+
+  it.todo('navigates back to the previous page of results');
 });
