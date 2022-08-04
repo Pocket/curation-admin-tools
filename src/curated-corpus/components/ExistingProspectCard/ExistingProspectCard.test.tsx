dae7c1a5530d927ccb418491c7656a7b8f97e5bb
Herraj Luhano <hluhano@mozilla.com>
2022-08-04 16:55:53 +0100

    fix typo from a merged pr

--- src/curated-corpus/components/ExistingProspectCard/ExistingProspectCard.test.tsx
+++ src/curated-corpus/components/ExistingProspectCard/ExistingProspectCard.test.tsx
@@ -168,7 +168,7 @@ describe('The ExistingProspectCard component', () => {
 
   it('should render schedule history component', async () => {
     /**
-     * We have separate units tests testing ScheduleHistory component in details in it's corresponding test file
+     * We have separate units tests testing ScheduleHistory component in details in its corresponding test file
      * This test only tests if the ScheduleHistory component renders within this component
      */
 
