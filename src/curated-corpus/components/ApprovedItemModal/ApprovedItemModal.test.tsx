b426e71f121aaa247ef81a364104ae3410a40c6b
Herraj Luhano <hluhano@mozilla.com>
2022-08-04 14:37:47 +0100

    add another test case

--- src/curated-corpus/components/ApprovedItemModal/ApprovedItemModal.test.tsx
+++ src/curated-corpus/components/ApprovedItemModal/ApprovedItemModal.test.tsx
@@ -42,7 +42,7 @@ describe('The ApprovedItemModal component', () => {
   const toggleModal = jest.fn();
   const onImageSave = jest.fn();
 
-  it('should render the component', () => {
+  it('should render the component with heading and approved item title', () => {
     render(
       <MockedProvider>
         <SnackbarProvider maxSnack={1}>
@@ -65,5 +65,28 @@ describe('The ApprovedItemModal component', () => {
 
     // should render the approved item's title if showItemTitle prop is true
     expect(screen.getByText(approvedItem.title)).toBeInTheDocument();
+  });
+
+  it('should render the component without heading and approved item title', () => {
+    render(
+      <MockedProvider>
+        <SnackbarProvider maxSnack={1}>
+          <ApprovedItemModal
+            approvedItem={approvedItem}
+            isOpen={isOpen}
+            onSave={onSave}
+            toggleModal={toggleModal}
+            isRecommendation={isRecommendation}
+            onImageSave={onImageSave}
+          />
+        </SnackbarProvider>
+      </MockedProvider>
+    );
+
+    // assert the modal is not rendered a heading
+    expect(screen.queryByText(heading)).toBeNull();
+
+    // assert that the approved item title is not rendered if showItemTitle prop is not passed in
+    expect(screen.queryByText(approvedItem.title)).toBeNull();
   });
 });
