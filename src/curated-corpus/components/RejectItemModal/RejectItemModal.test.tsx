843a0e92da5b136902f8bd63f887a11fff917b3e
Herraj Luhano <hluhano@mozilla.com>
2022-08-04 16:49:06 +0100

    chore(tests): [BACK] Add unit tests for RejectedItemModal component

---
+++ src/curated-corpus/components/RejectItemModal/RejectItemModal.test.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import { Prospect, ProspectType } from '../../../api/generatedTypes';
+import { ScheduledSurfaces } from '../../helpers/definitions';
+import { RejectItemModal } from './RejectItemModal';
+
+describe('The RejectItemModal component', () => {
+  const prospect: Prospect = {
+    id: 'test-id',
+    prospectId: 'test-prospect-id',
+    prospectType: ProspectType.Global,
+    scheduledSurfaceGuid: ScheduledSurfaces[0].guid,
+    url: 'www.test-prospect-url.com',
+    title: 'test-title',
+  };
+  const toggleModal = jest.fn();
+  const onSave = jest.fn();
+
+  it('should render successfully', () => {
+    render(
+      <RejectItemModal
+        prospect={prospect}
+        isOpen={true}
+        onSave={onSave}
+        toggleModal={toggleModal}
+      />
+    );
+
+    expect(
+      screen.getByText(/reject this item from inclusion in the curated corpus/i)
+    ).toBeInTheDocument();
+  });
+});
