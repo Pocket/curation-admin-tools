f721fbafc849174e247b38b4eadc36e4fd555641
Nina Pypchenko <22447785+nina-py@users.noreply.github.com>
2022-12-21 07:55:09 +1100

    feat(mui5): [BACK-1747] Migrate Curated Corpus Rejected page to MUI 5 (#978)
    - Updated the components used on this page & verified everything still works!

--- src/curated-corpus/components/RejectedItemSearchForm/RejectedItemSearchForm.tsx
+++ src/curated-corpus/components/RejectedItemSearchForm/RejectedItemSearchForm.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FormikHelpers, FormikValues, useFormik } from 'formik';
-import { Grid } from '@material-ui/core';
+import { Grid } from '@mui/material';
 import {
   Button,
   FormikSelectField,
--- src/curated-corpus/pages/RejectedPage/RejectedPage.tsx
+++ src/curated-corpus/pages/RejectedPage/RejectedPage.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Grid, Typography } from '@material-ui/core';
+import { Grid, Typography } from '@mui/material';
 import { FormikValues } from 'formik';
 import { config } from '../../../config';
 import {
