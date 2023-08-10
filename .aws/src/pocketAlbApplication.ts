9a4c162d3936b97d2b38b2096649cc950dd073a8
revelyn-mozilla <revelyn@mozilla.com>
2023-08-10 13:37:37 -0400

    add escalation

--- .aws/src/pocketAlbApplication.ts
+++ .aws/src/pocketAlbApplication.ts
@@ -32,10 +32,10 @@ function createPagerDuty(scope: Construct) {
     prefix: config.prefix,
     service: {
       criticalEscalationPolicyId: incidentManagement
-        .get('policy_backend_product_critical_id')
+        .get('policy_default_critical_id')
         .toString(),
       nonCriticalEscalationPolicyId: incidentManagement
-        .get('policy_backend_product_non_critical_id')
+        .get('policy_default_non_critical_id')
         .toString(),
     },
   });
