677755071b8bff555d3be0d252373b22cb1fb6bc
Jonathan Petto <jpetto@users.noreply.github.com>
2024-04-05 13:11:34 -0500

    feat: send actionScreen value to relevant mutations

--- src/api/mutations/createScheduledCorpusItem.ts
+++ src/api/mutations/createScheduledCorpusItem.ts
@@ -9,6 +9,7 @@ export const createScheduledCorpusItem = gql`
     $source: ScheduledItemSource!
     $reasons: String
     $reasonComment: String
+    $actionScreen: ActionScreen
   ) {
     createScheduledCorpusItem(
       data: {
@@ -18,6 +19,7 @@ export const createScheduledCorpusItem = gql`
         source: $source
         reasons: $reasons
         reasonComment: $reasonComment
+        actionScreen: $actionScreen
       }
     ) {
       externalId
