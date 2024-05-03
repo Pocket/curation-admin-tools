fca143c098eaedfa89ef6996da4553ba49c10e94
Nina Pypchenko <22447785+nina-py@users.noreply.github.com>
2024-05-03 18:17:49 +1000

    chore(tests): [MC-991] Improve test coverage for AprovedItemForm
    - Updated EditCorpusItemAction tests to add test coverage around
    not providing a value for `datePublished`
    
    Reference: https://mozilla-hub.atlassian.net/browse/MC-991

--- src/curated-corpus/components/actions/EditCorpusItemAction/EditCorpusItemAction.test.tsx
+++ src/curated-corpus/components/actions/EditCorpusItemAction/EditCorpusItemAction.test.tsx
@@ -4,9 +4,13 @@ import { render, screen } from '@testing-library/react';
 import { SnackbarProvider } from 'notistack';
 import { EditCorpusItemAction } from './EditCorpusItemAction';
 import { getTestApprovedItem } from '../../../helpers/approvedItem';
-import { successMock } from '../../../integration-test-mocks/updateApprovedItem';
+import {
+  successMock,
+  successMockSansDatePublished,
+} from '../../../integration-test-mocks/updateApprovedItem';
 import userEvent from '@testing-library/user-event';
 import { apolloCache } from '../../../../api/client';
+import { ActionScreen } from '../../../../api/generatedTypes';
 
 describe('The EditCorpusItemAction', () => {
   let mocks: MockedResponse[] = [];
@@ -21,6 +25,7 @@ describe('The EditCorpusItemAction', () => {
             item={getTestApprovedItem()}
             toggleModal={jest.fn()}
             modalOpen={true}
+            actionScreen={ActionScreen.Corpus}
           />
         </SnackbarProvider>
       </MockedProvider>
@@ -43,6 +48,36 @@ describe('The EditCorpusItemAction', () => {
             item={getTestApprovedItem()}
             toggleModal={jest.fn()}
             modalOpen={true}
+            actionScreen={ActionScreen.Corpus}
+          />
+        </SnackbarProvider>
+      </MockedProvider>
+    );
+
+    // The most basic of integration tests - we simply send through
+    // the corpus item without any actual edits.
+    userEvent.click(screen.getByText(/save/i));
+
+    expect(
+      await screen.findByText(/curated item .* successfully updated/i)
+    ).toBeInTheDocument();
+  });
+
+  it('completes the action successfully for an item with no `datePublished`', async () => {
+    mocks = [successMockSansDatePublished];
+
+    // Unset the publication date in test data
+    const curatedItem = getTestApprovedItem();
+    curatedItem.datePublished = null;
+
+    render(
+      <MockedProvider mocks={mocks} cache={apolloCache}>
+        <SnackbarProvider maxSnack={3}>
+          <EditCorpusItemAction
+            item={curatedItem}
+            toggleModal={jest.fn()}
+            modalOpen={true}
+            actionScreen={ActionScreen.Corpus}
           />
         </SnackbarProvider>
       </MockedProvider>
