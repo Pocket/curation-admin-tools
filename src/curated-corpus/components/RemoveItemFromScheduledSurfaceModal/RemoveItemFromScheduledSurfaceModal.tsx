585d8637e6effd3dff626cdc7ab7f9cf7715dd71
Nina Pypchenko <22447785+nina-py@users.noreply.github.com>
2024-05-16 09:30:52 +1000

    feat(scheduling): [MC-859] Provide a reason for unscheduling an item on New Tab DE (#1200)
    - Enabled unscheduling/removal reasons for NEW_TAB_DE_DE
    
    - Updated tests.

--- src/curated-corpus/components/RemoveItemFromScheduledSurfaceModal/RemoveItemFromScheduledSurfaceModal.tsx
+++ src/curated-corpus/components/RemoveItemFromScheduledSurfaceModal/RemoveItemFromScheduledSurfaceModal.tsx
@@ -20,8 +20,11 @@ export const RemoveItemFromScheduledSurfaceModal: React.FC<
   RemoveItemFromScheduledSurfaceModalProps
 > = (props): JSX.Element => {
   const { item, isOpen, onSave, toggleModal } = props;
-  // whether to show the remove item form
-  const isSurfaceEN = item.scheduledSurfaceGuid === 'NEW_TAB_EN_US';
+  // Surfaces for which unsheduling/removal reasons are required
+  const surfaces = ['NEW_TAB_EN_US', 'NEW_TAB_DE_DE'];
+
+  // Whether to show the remove item form
+  const showRemovalReasons = surfaces.includes(item.scheduledSurfaceGuid);
 
   return (
     <Modal
@@ -31,25 +34,24 @@ export const RemoveItemFromScheduledSurfaceModal: React.FC<
       }}
     >
       <Grid container spacing={2}>
-        {/*<h2>Remove this item from this scheduled surface</h2>*/}
-        {isSurfaceEN && (
-          <Grid item xs={12}>
-            <h2>Reason(s) for Unscheduling this item</h2>
-            <Box mb={1}>
-              <Typography variant="subtitle1">
-                <em>Title</em>: {item.approvedItem.title}
-              </Typography>
-            </Box>
-          </Grid>
+        {showRemovalReasons && (
+          <>
+            <Grid item xs={12}>
+              <h2>Reason(s) for Unscheduling this item</h2>
+              <Box mb={1}>
+                <Typography variant="subtitle1">
+                  <em>Title</em>: {item.approvedItem.title}
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid item xs={12}>
+              <Box p={3} mt={-3.5}>
+                <RemoveItemForm onSubmit={onSave} onCancel={toggleModal} />
+              </Box>
+            </Grid>
+          </>
         )}
-        {isSurfaceEN && (
-          <Grid item xs={12}>
-            <Box p={3} mt={-3.5}>
-              <RemoveItemForm onSubmit={onSave} onCancel={toggleModal} />
-            </Box>
-          </Grid>
-        )}
-        {!isSurfaceEN && (
+        {!showRemovalReasons && (
           <Grid item xs={12}>
             <h2>Remove this item from this scheduled surface</h2>
             <RemoveItemFromScheduledSurfaceForm
