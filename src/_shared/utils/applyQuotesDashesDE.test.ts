745753c8ffc4341b77d8f38041e690ca2fbadeaa
Katerina Chinnappan <kchinnappan@mozilla.com>
2024-06-13 11:12:58 -0700

    MC-1190: fix German quotes / dashes formatting (#1205)
    * MC-1190: fix German quotes / dashes formatting

--- src/_shared/utils/applyQuotesDashesDE.test.ts
+++ src/_shared/utils/applyQuotesDashesDE.test.ts
@@ -18,7 +18,7 @@ describe('applyQuotesDashesDE', () => {
       `“Nicht eine mehr”: Diese spanische Netflix-Serie ist ein Mix aus “Tote Mädchen lügen nicht” und “Élite” – das musst du darüber wissen`,
     );
     expect(result).toEqual(
-      '„Nicht eine mehr”: Diese spanische Netflix-Serie ist ein Mix aus „Tote Mädchen lügen nicht” und „Élite” — das musst du darüber wissen',
+      '„Nicht eine mehr”: Diese spanische Netflix-Serie ist ein Mix aus „Tote Mädchen lügen nicht” und „Élite” – das musst du darüber wissen',
     );
   });
   it('Replaces opening « with „', () => {
@@ -29,9 +29,13 @@ describe('applyQuotesDashesDE', () => {
     const result = applyQuotesDashesDE("Here's to the great ones!»");
     expect(result).toEqual("Here's to the great ones!”");
   });
-  it('Replaces « with „ and » with “', () => {
+  it("Replaces «Here's to the great ones!» with „Here's to the great ones!”", () => {
     const result = applyQuotesDashesDE("«Here's to the great ones!»");
     expect(result).toEqual("„Here's to the great ones!”");
+  });
+  it('Replaces »example« with „example”', () => {
+    const result = applyQuotesDashesDE('»example«');
+    expect(result).toEqual('„example”');
   });
   it('Replaces opening " with „', () => {
     const result = applyQuotesDashesDE('"Here\'s to the great ones!');
@@ -49,24 +53,24 @@ describe('applyQuotesDashesDE', () => {
     const result = applyQuotesDashesDE('“Here\'s to the great ones!"');
     expect(result).toEqual("„Here's to the great ones!”");
   });
-  it('Replaces short dash (with whitespaces) with long em dash', () => {
+  it('Replaces short dash (with whitespaces) with long en dash', () => {
     const result = applyQuotesDashesDE('"Here\'s to the great - ones!"');
-    expect(result).toEqual("„Here's to the great — ones!”");
+    expect(result).toEqual("„Here's to the great – ones!”");
   });
-  it('Replaces en dash (–) (with whitespaces) with long em dash', () => {
+  it('Replaces long em dash (–) (with whitespaces) with long en dash', () => {
     const result = applyQuotesDashesDE(
-      '"Meeresregionen – in die pelagischen Zonen – verlegt"',
+      '"Meeresregionen — in die pelagischen Zonen — verlegt"',
     );
     expect(result).toEqual(
-      '„Meeresregionen — in die pelagischen Zonen — verlegt”',
+      '„Meeresregionen – in die pelagischen Zonen – verlegt”',
     );
   });
-  it('Should not replace short dash (-) with long em dash (—) if no whitespaces in short dash', () => {
+  it('Should not replace short dash (-) with long en dash (–) if no whitespaces in short dash', () => {
     const result = applyQuotesDashesDE('"Here\'s to the great-ones!"');
     expect(result).toEqual("„Here's to the great-ones!”");
   });
-  it('Should not replace en dash (–) with long em dash (—) if no whitespaces in en dash', () => {
-    const result = applyQuotesDashesDE('"Here\'s to the great–ones!"');
-    expect(result).toEqual("„Here's to the great–ones!”");
+  it('Should not replace em dash (—) with long en dash (–) if no whitespaces in em dash', () => {
+    const result = applyQuotesDashesDE('"Here\'s to the great—ones!"');
+    expect(result).toEqual("„Here's to the great—ones!”");
   });
 });
