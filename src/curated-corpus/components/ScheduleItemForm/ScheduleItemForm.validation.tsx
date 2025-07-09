cc36856ad417094253a403f94ff89c842398af43
Jeremy Walters <jeremywa@proton.me>
2025-07-09 10:56:23 -0400

    feat: remove manual add reason requirement

--- src/curated-corpus/components/ScheduleItemForm/ScheduleItemForm.validation.tsx
+++ src/curated-corpus/components/ScheduleItemForm/ScheduleItemForm.validation.tsx
@@ -64,47 +64,7 @@ export const getValidationSchema = (
         .max(100, 'Reason is too long, cannot exceed 100 characters.'), // max 100 chars for now
     })
     .test('manualScheduleReason', '', (obj) => {
-      // Only ever validate these fields if they are shown to the user
-      // Currently this is limited to manually scheduling items onto the EN_US New Tab surface.
-      if (showManualScheduleReasons) {
-        // If the "Other" checkbox was selected but no reason entered, fail validation
-        if (obj['OTHER'] && obj['reasonComment'] === undefined) {
-          return new yup.ValidationError(
-            'Please provide a comment for scheduling this item manually.',
-            null,
-            'reasonComment',
-          );
-        }
-        // If the "Other" checkbox was NOT selected but a reason was entered, fail validation
-        if (!obj['OTHER'] && obj['reasonComment']) {
-          return new yup.ValidationError(
-            'Please select the "OTHER" reason checkbox.',
-            null,
-            'manualScheduleReason',
-          );
-        }
-        // If at least one checkbox was selected & above conditions satisfied, pass validation
-        if (
-          obj[ManualScheduleReason.Evergreen] ||
-          obj[ManualScheduleReason.FormatDiversity] ||
-          obj[ManualScheduleReason.PublisherDiversity] ||
-          obj[ManualScheduleReason.TimeSensitiveExplainer] ||
-          obj[ManualScheduleReason.TimeSensitiveNews] ||
-          obj[ManualScheduleReason.TopicDiversity] ||
-          obj[ManualScheduleReason.Trending] ||
-          obj[ManualScheduleReason.UnderTheRadar] ||
-          (obj['OTHER'] && obj['reasonComment'])
-        ) {
-          return true;
-        }
-
-        return new yup.ValidationError(
-          'Please choose at least one reason to schedule this item manually.',
-          null,
-          'manualScheduleReason',
-        );
-      } else {
-        return true;
-      }
+      // Manual schedule reasons are no longer required - always pass validation
+      return true;
     });
 };
